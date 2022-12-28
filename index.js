c534c2dad0c54271dc21ff21587277d4ba4698b3
myo-i <Imiyo0521@gmail.com>
2022-12-29 01:46:45 +0900

    ✨ npx tsc index.tsでtsファイルをコンパイルしてindex.jsを自動生成

---
+++ index.js
@@ -0,0 +1,17 @@
+var button = document.querySelector("button");
+// 末尾に感嘆符を付けることで確実に要素を取得させることができる。
+// つまりnullを返さない。
+// さらに下記は入力要素ということが分かっているのでどのタイプになるのかを知らせることができる。
+var input1 = document.getElementById("num1");
+var input2 = document.getElementById("num2");
+function add(num1, num2) {
+    if (typeof num1 === "number" && typeof num2 === "number") {
+        return num1 + num2;
+    }
+    else {
+        return +num1 + +num2;
+    }
+}
+button.addEventListener("click", function () {
+    console.log(add(+input1.value, +input2.value));
+});
