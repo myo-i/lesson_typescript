ac691b4927fc742d0f75090bdc19963170fc1d8d
myo-i <Imiyo0521@gmail.com>
2022-12-30 01:02:43 +0900

    ✨ 型チェック

--- basics/basic.ts
+++ basics/basic.ts
@@ -1,4 +1,8 @@
 function add(num1: number, num2: number) {
+    // 下記のような型チェックはtypescriptでは冗長になる（言語が型チェックしているため）
+    // if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+    //     throw new Error('Input type number!!')
+    // }
     return num1 + num2
 }
 
