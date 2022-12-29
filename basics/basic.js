79038dd2bb130823d681fccf6a1261617847f2fe
myo-i <Imiyo0521@gmail.com>
2022-12-30 01:17:41 +0900

    ✨ number, string, bool

--- basics/basic.js
+++ basics/basic.js
@@ -1,11 +1,18 @@
-function add(num1, num2) {
+function add(num1, num2, bool, answer) {
     // 下記のような型チェックはtypescriptでは冗長になる（言語が型チェックしているため）
     // if (typeof num1 !== 'number' || typeof num2 !== 'number') {
     //     throw new Error('Input type number!!')
     // }
+    // 計算も文字列と判定されてしまうので変数に格納
+    var result = num1 + num2;
+    if (bool) {
+        console.log(answer + result);
+    }
     return num1 + num2;
 }
-var number1 = 5;
+var number1 = 5; // jsやtsでは数値はデフォルトでfloatのため、5も5.0も変わらない
 var number2 = 2.8;
-var result = add(number1, number2);
-console.log(result);
+var putResult = true;
+var answer = "The answer is ";
+add(number1, number2, putResult, answer);
+// console.log(result)
--- basics/basic.ts
+++ basics/basic.ts
@@ -1,13 +1,23 @@
-function add(num1: number, num2: number) {
+function add(num1: number, num2: number, bool: boolean, answer: string) {
     // 下記のような型チェックはtypescriptでは冗長になる（言語が型チェックしているため）
     // if (typeof num1 !== 'number' || typeof num2 !== 'number') {
     //     throw new Error('Input type number!!')
     // }
+
+    // 計算も文字列と判定されてしまうので変数に格納
+    const result = num1 + num2
+    if(bool){
+        console.log(answer + result)
+    }
     return num1 + num2
 }
 
-const number1 = 5;
-const number2 = 2.8
 
-const result = add(number1, number2)
-console.log(result)
+// 型定義は行わない
+const number1 = 5; // jsやtsでは数値はデフォルトでfloatのため、5も5.0も変わらない
+const number2 = 2.8
+const putResult = true;
+const answer = "The answer is "
+
+add(number1, number2, putResult, answer)
+// console.log(result)
