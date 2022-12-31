8b487910e426ef089240cb211c06b21daedc83c4
myo-i <Imiyo0521@gmail.com>
2023-01-01 06:51:29 +0900

    ✨ literaltypeで実装したコードをenumで実装してみる

--- basics/enum.js
+++ basics/enum.js
@@ -25,3 +25,37 @@ console.log(information2);
 if (information2.weather === Weather.SUNNY) {
     console.log("Good weather");
 }
+// literaltypeで書いたコードをenumで実装してみる
+var As;
+(function (As) {
+    As[As["NUMBER"] = 0] = "NUMBER";
+    As[As["TEXT"] = 1] = "TEXT";
+})(As || (As = {}));
+// function c(
+//     input1: number | string, 
+//     input2: number | string, 
+//     option: "number" | "text") {
+//     let result
+//     if(typeof input1 === "number" && typeof input2 === "number" || option === "number"){
+//         result = +input1 + +input2
+//     } else {
+//         result = input1.toString() + input2.toString()
+//     }
+//     return result
+// }
+function cE(input1, input2, option) {
+    var result;
+    if (typeof input1 === "number" && typeof input2 === "number" || option === As.NUMBER) {
+        result = +input1 + +input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
+}
+var cENumber = cE(102, 231, As.NUMBER);
+console.log(cENumber);
+var cENumber2 = cE("917", "354", As.NUMBER);
+console.log(cENumber);
+var cEString = cE("good ", "day", As.TEXT);
+console.log(cEString);
--- basics/enum.ts
+++ basics/enum.ts
@@ -31,3 +31,46 @@ console.log(information2)
 if (information2.weather === Weather.SUNNY){
     console.log("Good weather");
 }
+
+
+// literaltypeで書いたコードをenumで実装してみる
+enum As {
+    NUMBER,
+    TEXT
+}
+
+// function c(
+//     input1: number | string, 
+//     input2: number | string, 
+//     option: "number" | "text") {
+//     let result
+//     if(typeof input1 === "number" && typeof input2 === "number" || option === "number"){
+//         result = +input1 + +input2
+//     } else {
+//         result = input1.toString() + input2.toString()
+//     }
+//     return result
+// }
+
+function cE(
+    input1: number | string, 
+    input2: number | string, 
+    option: As) {
+    let result
+    if(typeof input1 === "number" && typeof input2 === "number" || option === As.NUMBER){
+        result = +input1 + +input2
+    } else {
+        result = input1.toString() + input2.toString()
+    }
+    return result
+}
+
+
+const cENumber = cE(102, 231, As.NUMBER)
+console.log(cENumber)
+
+const cENumber2 = cE("917", "354", As.NUMBER)
+console.log(cENumber)
+
+const cEString = cE("good ", "day", As.TEXT)
+console.log(cEString)
