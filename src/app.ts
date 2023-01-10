cbe6914d646aaf72961517d77fb3000507700685
myo-i <Imiyo0521@gmail.com>
2023-01-10 23:54:02 +0900

    ✨ titleフォームに文字列を入力するとコンソールに表示されるようにした

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ class ProjectInput {
     templateEl: HTMLTemplateElement;
     divIdApp: HTMLDivElement;
     element: HTMLFormElement;
+    eleIdTitle: HTMLInputElement;
+    eleIdDescription: HTMLInputElement;
+    eleIdPeople: HTMLInputElement;
 
     constructor() {
         // as ~~ でnullを許容せず、この型を持つことを保証している。「")!」 でも可
@@ -12,8 +15,24 @@ class ProjectInput {
 
 
         const templateElNode = document.importNode(this.templateEl.content, true);
-        this.element = templateElNode.firstElementChild as HTMLFormElement
+        this.element = templateElNode.firstElementChild as HTMLFormElement;
+        this.element.id = "user-input";
+
+        this.eleIdTitle = this.element.querySelector("#title") as HTMLInputElement;
+        this.eleIdDescription = this.element.querySelector("#description") as HTMLInputElement;
+        this.eleIdPeople = this.element.querySelector("#people") as HTMLInputElement;
+
+        this.configure();
         this.attach();
+    }
+
+    private submitHandler(event: Event) {
+        event.preventDefault();
+        console.log(this.eleIdTitle.value);
+    }
+
+    private configure() {
+        this.element.addEventListener("submit", this.submitHandler.bind(this));
     }
 
     private attach() {
