0c2c1e08332b4415ab92943e703f3880310e6e25
myo-i <Imiyo0521@gmail.com>
2023-01-30 04:02:32 +0900

    ✨ factory function

--- decorator/01/decorator.js
+++ decorator/01/decorator.js
@@ -4,16 +4,22 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-function Logger(constructor) {
-    console.log("Logging....");
+function Logger(logString) {
+    return function (constructor) {
+        console.log(logString);
+        console.log(constructor);
+    };
 }
+// デコレーターはクラスが定義されたときに実行される
+// そのためこのプログラムで最初に出力されるのはLogging...となる
+// クラスでコンストラクタを作成しなくても出力される
 var Person = /** @class */ (function () {
     function Person() {
         this.name = "Max";
         console.log("Person Object.....");
     }
     Person = __decorate([
-        Logger
+        Logger("LOGGING PERSON CLASS")
     ], Person);
     return Person;
 }());
--- decorator/01/decorator.ts
+++ decorator/01/decorator.ts
@@ -1,12 +1,14 @@
-function Logger(constructor: Function) {
-    console.log("Logging....");
-    console.log(constructor);
+function Logger(logString: string){
+    return function(constructor: Function) {
+        console.log(logString);
+        console.log(constructor);
+    };
 }
 
 // デコレーターはクラスが定義されたときに実行される
 // そのためこのプログラムで最初に出力されるのはLogging...となる
 // クラスでコンストラクタを作成しなくても出力される
-@Logger
+@Logger("LOGGING PERSON CLASS")
 class Person {
     name = "Max";
 
